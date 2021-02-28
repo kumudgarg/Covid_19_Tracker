dab84ae6ccdeb08f7c4fb80b25facc4bf08e2cf0
kumudgarg <kumud50garg@gmail.com>
2021-03-01 00:40:56 +0530

    get the remote daily covid data into local

---
+++ src/test/java/com/coderkumud/covid_19_tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderkumud.covid_19_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
